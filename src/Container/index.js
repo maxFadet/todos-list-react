d945b09c5121e5cb4d49253855cf0093b1983c70
Maksim Padrabinkin <averybodymax@gmail.com>
2024-06-03 14:12:10 +0200

    reanamed coneiner to container

---
+++ src/Container/index.js
@@ -0,0 +1,15 @@
+import "./style.css";
+
+const Container = ({ title, body, extraHeadercontent }) => (
+    <section className="container">
+        <div className="container__header container__header--headerWithButtons">
+            <h2 className="container__title">{title}</h2>
+            {extraHeadercontent}
+        </div>
+        <div className="container__body">
+            {body}
+        </div>
+    </section>
+);
+
+export default Container;
