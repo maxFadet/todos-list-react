5960f34e37ef748be7b1dac8c93163d9022e6fe3
Maksim Padrabinkin <averybodymax@gmail.com>
2024-06-20 21:28:18 +0200

    Changes related to Redux integration: unnecessary functions have been removed. LocalStorage has been completely removed.

--- src/useTasksWithLocalStorage.js
+++ src/useTasksWithLocalStorage.js
@@ -9,31 +9,10 @@ export const useTasksWithLocalStorage = () => {
     ];
   });
 
-  const [hideDone, setHideDone] = useState(() => {
-    const savedHideDone = localStorage.getItem("hideDone");
-    return savedHideDone ? JSON.parse(savedHideDone) : false;
-  });
-
-  useEffect(() => {
-    localStorage.setItem("tasks", JSON.stringify(tasks));
-  }, [tasks]);
-
-  useEffect(() => {
-    localStorage.setItem("hideDone", JSON.stringify(hideDone));
-  }, [hideDone]);
-
   const removeTask = (id) => {
     setTasks(tasks => tasks.filter(task => task.id !== id));
   };
 
-  const toggleTaskDone = (id) => {
-    setTasks(tasks => tasks.map(task => {
-      if (task.id === id) {
-        return { ...task, done: !task.done };
-      }
-      return task;
-    }));
-  };
 
   const setAllDone = () => {
     setTasks(tasks => tasks.map(task => ({
@@ -42,36 +21,8 @@ export const useTasksWithLocalStorage = () => {
     })));
   };
 
-  const addNewTask = (content) => {
-    setTasks(tasks => [
-      ...tasks,
-      {
-        content,
-        done: false,
-        id: tasks.length ? tasks[tasks.length - 1].id + 1 : 1,
-      },
-    ]);
-  };
-
-  const toggleHideDone = () => {
-    setHideDone(hideDone => !hideDone);
-  };
-
-  const clearLocalStorage = () => {
-    localStorage.removeItem("tasks");
-    localStorage.removeItem("hideDone");
-    setTasks([]);
-    setHideDone(false);
-  };
-
   return {
-    tasks,
-    hideDone,
     removeTask,
-    toggleTaskDone,
     setAllDone,
-    addNewTask,
-    toggleHideDone,
-    clearLocalStorage
   };
 };
