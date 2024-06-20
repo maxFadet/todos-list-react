2a4aa738a85e3b6a3ace703f08b4987bd22cfa33
Maksim Padrabinkin <averybodymax@gmail.com>
2024-06-20 21:22:23 +0200

    useDispatch is used to toggle the task state (completed or not)

--- src/features/tasks/TaskList/index.js
+++ src/features/tasks/TaskList/index.js
@@ -1,36 +1,39 @@
 import React from "react";
 import { List, Item, Content, Button } from "./styled";
+import { useSelector, useDispatch } from "react-redux";
+import { selectTasks, toggleTaskDone } from "../tasksSlice";
 
-const TaskList = ({
-  tasks,
-  hideDone,
-  removeTask,
-  toggleTaskDone
-}) => (
-  <List>
-    {tasks.map(task => (
-      <Item
-        key={task.id}
-        hidden={task.done && hideDone}
-      >
-        <Button
-          toggleDone
-          onClick={() => toggleTaskDone(task.id)}
-        >
-          {task.done ? "✓" : ""}
-        </Button>
-        <Content done={task.done}>
-          {task.content}
-        </Content>
-        <Button
-          remove
-          onClick={() => removeTask(task.id)}
-        >
-          ✗
-        </Button>
-      </Item>
-    ))}
-  </List>
-);
+const TaskList = ({ removeTask }) => {
+  const { tasks, hideDone } = useSelector(selectTasks);
+  const dispatch = useDispatch();
+  return (
+    (
+      <List>
+        {tasks.map(task => (
+          <Item
+            key={task.id}
+            hidden={task.done && hideDone}
+          >
+            <Button
+              toggleDone
+              onClick={() => dispatch(toggleTaskDone(task.id))}
+            >
+              {task.done ? "✓" : ""}
+            </Button>
+            <Content done={task.done}>
+              {task.content}
+            </Content>
+            <Button
+              remove
+              onClick={() => removeTask(task.id)}
+            >
+              ✗
+            </Button>
+          </Item>
+        ))}
+      </List>
+    )
+  );
+};
 
 export default TaskList;
