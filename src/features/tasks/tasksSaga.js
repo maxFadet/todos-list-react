1a59ff408af2feec0400d0b53a14960fe7d09032
Maksim Padrabinkin <averybodymax@gmail.com>
2024-06-24 20:58:51 +0200

    Added a delay effect to simulate a delay before loading sample tasks, and takeLatest, which will call fetchExampleTasksHandler only for the latest request

--- src/features/tasks/tasksSaga.js
+++ src/features/tasks/tasksSaga.js
@@ -1,9 +1,10 @@
-import { takeEvery, call, put } from "redux-saga/effects";
+import { takeLatest, call, put, delay } from "redux-saga/effects";
 import { fetchExampleTasks, setTasks } from "./tasksSlice";
 import { getExampleTasks } from "./getExampleTasks";
 
 function* fetchExmpleTasksHandler() {
     try {
+        yield delay(1000);
         const exampleTasks = yield call(getExampleTasks);
         yield put(setTasks(exampleTasks));
     } catch (error) {
@@ -12,5 +13,5 @@ function* fetchExmpleTasksHandler() {
 };
 
 export function* watchFetchExampleTasks() {
-    yield takeEvery(fetchExampleTasks.type, fetchExmpleTasksHandler);
+    yield takeLatest(fetchExampleTasks.type, fetchExmpleTasksHandler);
 };
