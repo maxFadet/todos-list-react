2d1eac38353164c1a37bbbb16f2d3fe65d3b9ab0
Maksim Padrabinkin <averybodymax@gmail.com>
2024-06-25 20:07:24 +0200

    Added subpages for tasks and about the author

--- src/App.js
+++ src/App.js
@@ -1,3 +1,26 @@
-import Tasks from "./features/tasks/Tasks"
+import { BrowserRouter, Link, Switch, Route } from "react-router-dom/cjs/react-router-dom.min";
+import Tasks from "./features/tasks/Tasks";
+import Author from "./features/author/Author";
 
-export default () => <Tasks />
+export default () => (
+    <BrowserRouter>
+        <nav>
+            <ul>
+                <li>
+                    <Link to="/zadania">Zadania</Link>
+                </li>
+                <li>
+                    <Link to="/autor">O autorze</Link>
+                </li>
+            </ul>
+            <Switch>
+                <Route path="/zadania">
+                    <Tasks />
+                </Route>
+                <Route path="/autor">
+                    <Author />
+                </Route>
+            </Switch>
+        </nav>
+    </BrowserRouter>
+);
---
+++ src/features/author/Author.js
@@ -0,0 +1,16 @@
+import Container from "../../common/Container";
+import Header from "../../common/Header";
+import Section from "../../common/Section";
+
+
+export default () => (
+    <Container>
+        <Header title="O autorze" />
+        <Section
+            title="Maksim Padrabinkin"
+            body={
+                <>nana</>
+            }
+        />
+    </Container>
+);
