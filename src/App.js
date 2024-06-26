6c1865111c85c24757fab6e3bd5486c3991d8aca
Maksim Padrabinkin <averybodymax@gmail.com>
2024-06-26 16:37:33 +0200

    Маршрут для страницы задачи по ID. Renamed some components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { HashRouter, Link, Switch, Route, Redirect } from "react-router-dom/cjs/react-router-dom.min";
-import Tasks from "./features/tasks/Tasks";
-import Author from "./features/author/Author";
+import TasksPage from "./features/tasks/TasksPage";
+import TaskPage from "./features/tasks/TaskPage";
+import AuthorPage from "./features/author/AuthorPage";
 
 export default () => (
     <HashRouter>
@@ -14,11 +15,14 @@ export default () => (
                 </li>
             </ul>
             <Switch>
+                <Route path="/zadania/:id">
+                    <TaskPage />
+                </Route>
                 <Route path="/zadania">
-                    <Tasks />
+                    <TasksPage />
                 </Route>
                 <Route path="/autor">
-                    <Author />
+                    <AuthorPage />
                 </Route>
                 <Route path="/">
                     <Redirect to="/zadania" />
