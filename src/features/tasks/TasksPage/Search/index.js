2c94e217da89cdf17bea3510f688209d46d1f785
Maksim Padrabinkin <averybodymax@gmail.com>
2024-06-28 13:02:36 +0200

    Search functionality for tasks has been configured

--- src/features/tasks/TasksPage/Search/index.js
+++ src/features/tasks/TasksPage/Search/index.js
@@ -1,15 +1,29 @@
-import { useLocation } from "react-router-dom/cjs/react-router-dom.min";
+import { useHistory, useLocation } from "react-router-dom";
 import Input from "../../input";
 
 
 export default () => {
     const location = useLocation();
+    const history = useHistory();
     const query = (new URLSearchParams(location.search)).get("szukaj");
+
+    const onInputChange = ({ target }) => {
+        const searchParams = new URLSearchParams(location.search);
+
+        if (target.value.trim() === "") {
+            searchParams.delete("szukaj");
+        } else {
+            searchParams.set("szukaj", target.value);
+        }
+
+        history.push(`${location.pathname}?${searchParams.toString()}`);
+    };
 
     return (
         <Input
             placeholder="Filtruj zadania"
             value={query || ""}
+            onChange={onInputChange}
         />
     )
 };
