c02b171dee21e4881053b7dfb9484de2720ca688
Maksim Padrabinkin <averybodymax@gmail.com>
2024-06-28 19:12:46 +0200

    The logic for checking tasks has been serviced

--- src/features/tasks/TaskPage/index.js
+++ src/features/tasks/TaskPage/index.js
@@ -15,9 +15,9 @@ function TaskPage() {
             <Header title="Szczegóły zadania" />
             <Section
                 title={task ? task.content : "Nie znaleziono zadania 😢"}
-                body={
+                body={!!task && (
                     <><strong>Ukończono:</strong> {task.done ? "Tak" : "Nie"}</>
-                }
+                )}
             />
         </Container>
     );
