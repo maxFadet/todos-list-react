c2686028ed31e8d186bec472ab4aa67d013f4c63
Maksim Padrabinkin <averybodymax@gmail.com>
2024-06-28 14:09:06 +0200

    The navigation has been moved to a common folder

--- src/App.js
+++ src/App.js
@@ -2,19 +2,13 @@ import { HashRouter, Switch, Route, Redirect } from "react-router-dom";
 import TasksPage from "./features/tasks/TasksPage";
 import TaskPage from "./features/tasks/TaskPage";
 import AuthorPage from "./features/author/AuthorPage";
-import { StyledNavLink } from "./styled";
+import Navigation from "./common/Navigation";
+
 
 export default () => (
     <HashRouter>
         <nav>
-            <ul>
-                <li>
-                    <StyledNavLink to="/zadania">Zadania</StyledNavLink>
-                </li>
-                <li>
-                    <StyledNavLink to="/autor">O autorze</StyledNavLink>
-                </li>
-            </ul>
+            <Navigation />
             <Switch>
                 <Route path="/zadania/:id">
                     <TaskPage />
---
+++ src/common/Navigation/index.js
@@ -0,0 +1,14 @@
+import { StyledNavLink } from "./styled";
+
+const Navigation = () => {
+    <ul>
+        <li>
+            <StyledNavLink to="/zadania">Zadania</StyledNavLink>
+        </li>
+        <li>
+            <StyledNavLink to="/autor">O autorze</StyledNavLink>
+        </li>
+    </ul>
+}
+
+export default Navigation;
