06594ae4694ff1040b701071ae8f9bd82eda51f2
Maksim Padrabinkin <averybodymax@gmail.com>
2024-07-17 16:41:56 +0200

    Simplified the code

--- src/features/tasks/TasksPage/queryParameters.js
+++ src/features/tasks/TasksPage/queryParameters.js
@@ -1,10 +1,9 @@
 import { useLocation, useHistory } from "react-router-dom";
 
-export const useQueryParameter = (searchQueryParamName) => {
+export const useQueryParameter = key => {
     const location = useLocation();
-    const query = new URLSearchParams(location.search).get(searchQueryParamName);
-
-    return query;
+    const searchParams = new URLSearchParams(location.search);
+    return searchParams.get(key);
 };
 
 export const useReplaceQueryParameter = () => {
@@ -19,7 +18,7 @@ export const useReplaceQueryParameter = () => {
         } else {
             searchParams.set(key, value);
         }
-
-        history.push(`${location.pathname}?${searchParams.toString()}`);
+        const newSearch = searchParams.toString();
+        history.push(`${location.pathname}?${newSearch}`);
     };
 };
