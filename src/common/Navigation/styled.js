fc6615cf9a873a6fbe7142f425e0477fa1db1556
Maksim Padrabinkin <averybodymax@gmail.com>
2024-11-05 11:37:26 +0100

    Underline for navigational when pointing

--- src/common/Navigation/styled.js
+++ src/common/Navigation/styled.js
@@ -2,12 +2,29 @@ import styled from "styled-components";
 import { NavLink } from "react-router-dom";
 
 export const StyledNavLink = styled(NavLink)`
+    position: relative;
     padding: 20px;
     text-decoration: ${({ theme }) => theme.styles.removeStyle};
     color: ${({ theme }) => theme.colors.white};
-    
+    transition: color 0.3s;
+
     &.active {
         font-weight: 700;
+    }
+
+    &::after {
+        content: "";
+        position: absolute;
+        left: 0;
+        bottom: 5px;
+        width: 0;
+        height: 2px;
+        background-color: ${({ theme }) => theme.colors.white};
+        transition: width 0.3s ease;
+    }
+
+    &:hover::after {
+        width: 100%;
     }
 `;
 
